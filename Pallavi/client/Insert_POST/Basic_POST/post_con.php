33574fd6855ca0c74ae1870b2be080bc434e6a02
Pallavi Kamble <pallavikamble264@gmail.com>
2020-04-23 15:50:42 +0530

    For granting permissions
    requesting permissions

--- Pallavi/client/Insert_POST/Basic_POST/post_con.php
+++ Pallavi/client/Insert_POST/Basic_POST/post_con.php
@@ -1,11 +1,11 @@
 
 
-<?php
-//Establishing connection with the database
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'xyz'); //where books is the database name
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
+
+<?php
+	$server = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "xyz";
+	$conn = mysqli_connect($server, $user, $pass, $db);
 ?>
