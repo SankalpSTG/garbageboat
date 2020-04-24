f7954bdbaafa1e64990e367e464aaf1d86731450
Bhagwan Toksha <bhagwan.toksha@mit.asia>
2020-04-24 12:49:28 +0530

    Updated Web App Template

---
+++ Common/WebApp Template/index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+	<head>
+		<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+		<link rel="stylesheet" href="plugins/w3/w3.css">
+		<link rel="stylesheet" href="plugins/custom/custom_style.css">
+		<script src="plugins/custom/custom_javascript.js"></script>
+		<link rel="shortcut icon" href="resources/images/icon/YOUR ICON" />
+		<title>YOUR TITLE</title>
+	</head>
+	<body onresize="" onload="">
+		<header>
+		</header>
+		<div class="c-container">
+		</div>
+		<footer>
+		</footer>
+	</body>
+</html>
