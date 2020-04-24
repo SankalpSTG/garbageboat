9ed4d7fc04762433128ef42503728a71420e7248
Bhagwan Toksha <bhagwan.toksha@mit.asia>
2020-04-24 18:11:12 +0530

    updated files

---
+++ Common/API/files/upload_image.inc.php
@@ -0,0 +1,32 @@
+<?php
+	function uploadFile($file, $runningno, $path){
+		$target_dir = $_SERVER["DOCUMENT_ROOT"]."/garbageboat/data/images/".$path;
+		$target_file = $target_dir.basename($file["name"]);
+		$check = json_encode(getImageSize($file["tmp_name"]));
+		if($check === false){
+			return 0;
+		}else{
+			if(file_exists($target_file)){
+				return 2;
+			}else{
+				if($file["size"] > 500000) {
+					return 3;
+				}else{
+					$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+					if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+						return 4;
+					}else{
+						
+						$target_file = $target_dir.($runningno+1).".".$imageFileType;
+						if (move_uploaded_file($file["tmp_name"], $target_file)) {
+							return 1;
+						} else {
+							return 5;
+						}
+					}
+				}
+			}
+		}
+	}
+	echo uploadFile($_FILES["fileToUpload"], 1, "");
+?>
