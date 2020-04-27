bb8471c1af153647a9af22c34dce6fd3864aaa46
Bhagwan Toksha <bhagwan.toksha@mit.asia>
2020-04-27 16:27:39 +0530

    Updated Sankalp Garbage Server files

---
+++ Sankalp/garbageboat/api/v1/authentication/Generate_token.php
@@ -0,0 +1,40 @@
+<?php
+require ("../database/dbh.inc.php");
+require ("../other/encdec.php");
+	
+	$message["error"] = true;
+	$message["error_message"] = "Required all parameters";	
+	if(isset($_POST["serial_id"])){
+		$serial_id = mysqli_real_escape_string($conn, $_POST["serial_id"]);
+		if(empty($serial_id)){
+			$$message["error"] = true;
+			$message["error_message"] = "Required all parameters";
+			die(json_encode($message));
+		}else{
+			if(!is_numeric($serial_id)){
+				$message["error"] = true;
+				$message["error_message"] = "Invalid parameters";	
+				die(json_encode($message));
+			}else{
+				$otp = generateOtp(5);
+				$token_key = generateRandomString(10);
+				$token_iv = generateRandomString(10);
+				$token = encrypt_decrypt(0, $otp, $token_key, $token_iv);
+				$stmt = $conn->prepare("UPDATE credentials SET token = ?, token_key = ?, token_iv = ? WHERE serial_id = ?");
+				$stmt->bind_param("sssi", $token, $token_key, $token_iv, $serial_id);
+				if($stmt->execute()){
+					$stmt->close();
+					$message["data"] = $otp;
+					$message["error"] = false;
+					$message["error_message"] = "Success";
+					die(json_encode($message));
+				}else{
+					$message["error"] = true;
+					$message["error_message"] = "Failed to update";
+					die(json_encode($message));	
+				}
+			}			
+		}
+	}
+	die(json_encode($message));
+?>
---
+++ Sankalp/garbageboat/api/v1/authentication/login.php
@@ -0,0 +1,48 @@
+<?php
+require("../database/dbh.inc.php");
+	
+	$message["error"] = true;
+	$message["error_message"] = "Required all parameters";
+	if(isset($_POST["email_id"]) && isset($_POST["password"])){
+		$email_id = mysqli_real_escape_string($conn, $_POST["email_id"]);
+		$user_password = mysqli_real_escape_string($conn, $_POST["password"]);
+		if(empty($email_id) && empty($password)){
+			$message["error"] = true;
+			$message["error_message"] = "Required all parameters";
+			die(json_encode($message));
+		}else{
+		if((filter_var($email_id, FILTER_VALIDATE_EMAIL) === false) || !preg_match('/^(?=.*\d)(?=.*[@#\-_$%^&+=ยง!\?])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=ยง!\?]{8,20}$/',$user_password)){
+				$message["error"] = true;
+				$message["error_message"] = "Invalid parameters";	
+				die(json_encode($message));
+			}else{
+				$stmt = $conn->prepare("SELECT cred.serial_id, usr.password FROM users as usr, credentials as cred WHERE usr.credential_id = cred.serial_id and email_id = ?");
+				$stmt->bind_param("s", $email_id);
+				$stmt->execute();
+				$result = $stmt->get_result();
+				$stmt->close();
+				if(mysqli_num_rows($result) == 1){
+					$row = mysqli_fetch_assoc($result);
+					$serial_id = $row["serial_id"];
+					$password = $row["password"];
+					if(password_verify($user_password, $password)==true){
+						$message["data"] = $serial_id;
+						$message["error"] = false;
+						$message["error_message"] = "Success";
+						die(json_encode($message));
+					}else{
+					$message["error"] = true;
+					$message["error_message"] = "Invalid password";
+					die(json_encode($message));
+					}
+				}else{
+					$message["error"] = true;
+					$message["error_message"] = "Invalid user";
+					die(json_encode($message));
+				}
+			}	
+		}
+		
+	}
+	die(json_encode($message));
+?>
