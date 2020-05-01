28a8acc19876ddde710231b44df52a4612b7b5cf
Bhagwan Toksha <bhagwan.toksha@mit.asia>
2020-05-01 13:54:54 +0530

    Updated Sankalp Folder

--- Sankalp/server/garbageboat/webapi/v1/database/dbh.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$server = "localhost";
-	$user = "root";
-	$pass = "";
-	$db = "garbageboat";
-	$conn = mysqli_connect($server, $user, $pass, $db);
-	
-	$serveradd = "http://192.168.43.88/";
-?>
