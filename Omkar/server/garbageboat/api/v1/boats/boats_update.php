9607c6d6fd7d5bd94e96c5131fb80093090a8d81
Omkar <63800228+omkarprayag@users.noreply.github.com>
2020-05-03 00:23:30 +0530

    Create boats_update.php

---
+++ Omkar/server/garbageboat/api/v1/boats/boats_update.php
@@ -0,0 +1,41 @@
+<?php
+
+require("../database/dbh.inc.php");
+
+	$message["error"] = true;
+	$message["error_message"] = "Require all parameters";	
+	if(isset($_POST["registration_number"]) && isset($_POST["type"]) && isset($_POST["credential_id"]) && isset($_POST["pet_name"]) && isset($_POST["simulator_device_id"])){
+		$registration_number = (int)$_POST["registration_number"];
+		$credential_id = (int)$_POST["credential_id"];
+		$type = mysqli_real_escape_string($conn, $_POST["type"]);
+		$pet_name = mysqli_real_escape_string($conn, $_POST["pet_name"]);
+		$simulator_device_id = mysqli_real_escape_string($conn, $_POST["simulator_device_id"]);
+
+		if(empty($registration_number) || empty($type) || empty($credential_id) || empty($pet_name) || empty($simulator_device_id)){
+			$message["error"] = true;
+			$message["error_message"] = "Require all parameters";	
+			die(json_encode($message));
+		}else{
+			$stmt = $conn->prepare("SELECT registration_number, type FROM boats WHERE registration_number = ? and type = ? ");
+			$stmt->bind_param("is", $registration_number, $type);
+			$stmt->execute();
+			$result = $stmt->get_result();
+			$stmt->close();
+			if(mysqli_num_rows($result) == 1){
+				$stmt = $conn->prepare("UPDATE boats SET pet_name = ?, credential_id  = ?,  simulator_device_id = ?, verified = 1 WHERE registration_number = ?");
+				$stmt->bind_param("sisi", $pet_name, $credential_id, $simulator_device_id, $registration_number);
+				$stmt->execute();
+				$stmt->close();
+				$message["error"] = false;
+				$message["error_message"] = "Success";	
+				die(json_encode($message));
+			}else{
+				$message["error"] = true;
+				$message["error_message"] = "Invalid parameters";	
+				die(json_encode($message));
+			}
+		}
+		
+	}die(json_encode($message))
+
+?>
